fe7ce63487b87bf3f9a0b07957042a0e3ee83011
CAITLYN CHITWOOD <c.chitwood@wustl.edu>
2025-03-18 00:14:10 -0500

    scaffold package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sortscore',
+    version='0.1.0',
+    author='Caitlyn Chitwood',
+    author_email='c.chitwood@wustl.edu',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
